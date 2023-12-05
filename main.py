413d051a475f1565ff7fd33897af30bb10cf6898
Vartan <9574777@gmail.com>
2023-12-05 09:28:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ import rumps
 
 
 class WeatherApp(rumps.App):
-    API_KEY = 'YOUR_KEY'
-    DEFAULT_CITY = 'CITY,COUNTRY_ABBR'
+    """Получить ключ можно на сайте
+    https://openweathermap.org/api
+    API_KEY ваш ключ
+    DEFAULT_CITY ваш город
+    """
+    API_KEY = YOUR_KEY
+    DEFAULT_CITY = YOUR_CITY
 
     def __init__(self):
         super(WeatherApp, self).__init__("Weather Widget", 'Loading...')
         self.weather_manager = pyowm.OWM(self.API_KEY).weather_manager()
 
-    @property
-    def observation(self):
-        return self.weather_manager.weather_at_place(self.DEFAULT_CITY)
-
-    @property
-    def current_weather(self):
-        return self.observation.weather
-
-    @property
-    def temperature(self):
-        return self.current_weather.temperature('celsius')['temp']
-
-    @property
-    def description(self):
-        return self.current_weather.detailed_status.capitalize()
-
     @rumps.timer(1200)
     def update_weather(self, _):
-        self.title = f"{round(self.temperature)}°C {self.description}"
+        observation = self.weather_manager.weather_at_place(self.DEFAULT_CITY)
+        weather = observation.weather
+        icon = f'img/{weather.weather_icon_name[:-1]}.png'
+        temperature = round(weather.temperature('celsius')['temp'])
+        self.title = f"{temperature}°C"
+        self.icon = icon
 
     @rumps.clicked("Refresh data")
     def refresh_data(self, _):
