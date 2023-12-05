5e56a2cc7f9b781939e09b50ed0ecb629c76b9fe
Vartan <9574777@gmail.com>
2023-12-05 15:52:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import rumps
 
 
 class WeatherApp(rumps.App):
-    """Получить ключ можно на сайте
+    """You can get the key on the website
     https://openweathermap.org/api
-    API_KEY ваш ключ
-    DEFAULT_CITY ваш город
+    API_KEY your key
+    DEFAULT_CITY your city
     """
-    API_KEY = YOUR_KEY
-    DEFAULT_CITY = YOUR_CITY
+    API_KEY = HERE_API_KEY
+    DEFAULT_CITY = 'CITY, COUNTRY_ABBR'
 
     def __init__(self):
         super(WeatherApp, self).__init__("Weather Widget", 'Loading...')
@@ -18,11 +18,9 @@ class WeatherApp(rumps.App):
     @rumps.timer(1200)
     def update_weather(self, _):
         observation = self.weather_manager.weather_at_place(self.DEFAULT_CITY)
-        weather = observation.weather
-        icon = f'img/{weather.weather_icon_name[:-1]}.png'
-        temperature = round(weather.temperature('celsius')['temp'])
+        temperature = round(observation.weather.temperature('celsius')['temp'])
+        self.icon = f'img/{observation.weather.weather_icon_name[:-1]}.png'
         self.title = f"{temperature}°C"
-        self.icon = icon
 
     @rumps.clicked("Refresh data")
     def refresh_data(self, _):
